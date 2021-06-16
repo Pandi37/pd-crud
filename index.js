bdc12d871b27366e067242c6e8daca7d5beea02d
Your Name <you@example.com>
2021-06-16 11:45:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('./db')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+const port = process.env.PORT || 4000;
+
+dotenv.config()
+
+var postMessageRoutes = require('./controllers/postMessageController')
+
+
+var app = express()
+app.use(bodyParser.json())
+app.use(cors({origin:'http://localhost:3000'}))
+app.listen(port, () => {
+    console.log(`Server started at : ${port}`)
+})
+
+
+app.use('/postMessages',postMessageRoutes)
